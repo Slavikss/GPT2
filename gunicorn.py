438ab941e8aeafdb3e69a7b4ed2a4e8f0486148a
VGuch <ssslllava11@gmail.com>
2024-07-10 21:26:15 +0500

    update

--- gunicorn.py
+++ gunicorn.py
@@ -6,4 +6,4 @@ def child_exit(server, worker):
     GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
 
 bind = '0.0.0.0:8081'
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1 #multiprocessing.cpu_count() * 2 + 1
